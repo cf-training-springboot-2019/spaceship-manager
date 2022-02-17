f1512744a6782d0b12253addbcf80637f29ddf7d
Bruno Caetano <bruno.caetano1@vodafone.com>
2022-02-17 18:29:48 +0000

    [WEEK-1-START]:[minor tweaks]

--- src/main/java/com/training/springboot/spaceover/spaceship/manager/domain/response/outbound/GetSpaceShipResponse.java
+++ src/main/java/com/training/springboot/spaceover/spaceship/manager/domain/response/outbound/GetSpaceShipResponse.java
@@ -7,13 +7,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.hateoas.RepresentationModel;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class GetSpaceShipResponse extends RepresentationModel<GetSpaceShipResponse> {
+public class GetSpaceShipResponse {
 
     private Long id;
 
