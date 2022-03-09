37fb803de1f3ab73fa887464335d666df9fa6651
Bruno Caetano <bruno.caetano1@vodafone.com>
2022-03-09 18:17:36 +0000

    [WEEK-4-START]:[week-4 init]

--- src/main/java/com/training/springboot/spaceover/spaceship/manager/SpaceShipManagerApplication.java
+++ src/main/java/com/training/springboot/spaceover/spaceship/manager/SpaceShipManagerApplication.java
@@ -1,11 +1,9 @@
 package com.training.springboot.spaceover.spaceship.manager;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
-@SpringBootApplication
+//LT1-Integrate discovery client
+//LT2-Integrate configuration client
 public class SpaceShipManagerApplication {
 
     public static void main(String[] args) {
