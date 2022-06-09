37a3b2c7ed5f57cd0d9556d737e95de75cc5d033
Bruno Caetano <bruno.caetano1@vodafone.com>
2022-06-09 14:24:57 +0100

    init week branch

--- src/main/java/com/training/springboot/spaceover/spaceship/manager/SpaceShipManagerApplication.java
+++ src/main/java/com/training/springboot/spaceover/spaceship/manager/SpaceShipManagerApplication.java
@@ -1,9 +1,11 @@
 package com.training.springboot.spaceover.spaceship.manager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //LT1-Integrate discovery client
 //LT2-Integrate configuration client
+ @SpringBootApplication
 public class SpaceShipManagerApplication {
 
     public static void main(String[] args) {
